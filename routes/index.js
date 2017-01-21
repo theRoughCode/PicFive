f676785f252cd6542b933f736c96cc0f13305491
Matthew Ban Kim <matthewbankim2@gmail.com>
2017-01-21 12:46:48 -0500

    server now responds to POST

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var text = 'ServerUp'
 //GET homepage
 router.get('/', function(req, res) {
-  res.render('index', {
-      title: 'ServerUp'
-  });
-  console.log('GET - homepage')
+    res.render('index', {
+        title: text
+    });
+    console.log('GET - homepage')
 });
-/*
-router.post('/', function(req,m res) {
 
-}*/
+router.post('/', function(req, res) {
+    text = req.body.text
+    console.log('POST - text')
+    res.render('index', {
+        title: text
+    });
+});
 
 module.exports = router;

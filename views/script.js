c4342a3e9228787b84bffba37d68eb8976595830
Mohit Patel <omnivaliant@gmail.com>
2017-01-22 02:07:25 -0500

    Animated score count, and aligned table

--- views/script.js
+++ views/script.js
@@ -35,4 +35,16 @@ $(function () {
 
   // Add the compiled html to the page
   $('.leaderboardData').html(theCompiledHtml);
+
+  $('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
 });

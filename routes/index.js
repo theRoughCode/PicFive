a6d8ab7c3d30cda3c4a53b40002e64f0deec648d
Matthew Ban Kim <matthewbankim2@gmail.com>
2017-01-22 04:18:46 -0500

    fixes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,17 @@ var functions = require('../Functions');
 var models    = require('../Models');
 var Promise   = require('bluebird');
 var cool      = require('cool-ascii-faces');
+const EventEmitter = require('events');
+class MyEmitter extends EventEmitter {};
+const emitter = new MyEmitter();
+emitter.setMaxListeners(20);
 
 var buzzwords = ['car', 'banana', 'water', 'ice', 'tree'];
 //the query we will make on the database (max 50 players, sort largest > smallest)
 var query = score.find().sort({val: -1}).limit(50);
+var winner = ['N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A'];
+var winner_score = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var points = 0;
 //GET homepage
 router.get('/', function(req, res) {
     res.render('index');
@@ -20,8 +27,8 @@ router.post('/img', function(req, res) {
     var img = functions.base64(req.files.imgUp.data);
 
     //do function calls to get the player's score
-    var points;
     var scorePromise = functions.getScore(img, buzzwords);
+    points = scorePromise;
     var player = new score({
         name : 'username',
         val : points
@@ -33,24 +40,49 @@ router.post('/img', function(req, res) {
     .then(function(err, list) {
         if (err) return console.error(err);
         player.val = points;
-        player.save(function (err, data) {
+        player.save(function (points, err) {
             if (err) console.log(err);
-            console.log('Saved: ', data);
-        }).then(function(err, list) {
+            console.log('Saved: ', points);
+        });
+        if (err) return console.error(err);
+        query.exec(function(err, scores) {
             if (err) return console.error(err);
-            query.exec(function(err, scores) {
-                if (err) return console.error(err);
-                scores.forEach(function(scoreA) {
-                    // edit res to send leaderboard to client
-                    console.log(scoreA.name);
-                    console.log(scoreA.val);
-                })
+            scores.forEach(function(scoreA, winner_score, score) {
+                // edit res to send leaderboard to client
+                winner_score = scoreA.name;
+                score = scoreA.val;
+                console.log(scoreA.name);
+                console.log(scoreA.val);
             })
+            res.redirect('/views/leaderboards');
         })
-    });
-    //res.render leaderboard page
-    res.render('index');
+    })
+});
 
+router.get('/views/leaderboards', function(req, res) {
+    res.render('leaderboards', {
+        mainscore : points,
+        winner1 : score[0],
+        winner2 : score[1],
+        winner3 : score[2],
+        winner4 : score[3],
+        winner5 : score[4],
+        winner6 : score[5],
+        winner7 : score[6],
+        winner8 : score[7],
+        winner9 : score[8],
+        winner10 : score[9],
+        score1 : winner_score[0],
+        score2 : winner_score[1],
+        score3 : winner_score[2],
+        score4 : winner_score[3],
+        score5 : winner_score[4],
+        score6 : winner_score[5],
+        score7 : winner_score[6],
+        score8 : winner_score[7],
+        score9 : winner_score[8],
+        score10 : winner_score[9]
+    })
 });
 
 // GET 5 BUZZWORDS OF THE DAY
